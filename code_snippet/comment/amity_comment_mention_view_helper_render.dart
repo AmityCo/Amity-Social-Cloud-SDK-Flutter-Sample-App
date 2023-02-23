1c136bb907eef798e730e161b82f39fb8b2411b1
Trust Ratch <trustrat@gmail.com>
2023-02-23 14:19:35 +0700

    chore: fix duplicated gist id and rename comment with mention func (#142)

--- code_snippet/comment/amity_comment_mention_view_helper_render.dart
+++ code_snippet/comment/amity_comment_mention_view_helper_render.dart
@@ -2,12 +2,12 @@ import 'package:amity_sdk/amity_sdk.dart';
 
 class AmityCommentMentionViewHelperRender {
   /* begin_sample_code
-    gist_id: a6801145f042d65de89b2cde3042931e
+    gist_id: f4e91a73045d2278554ba9e8daf27dfc
     filename: AmityCommentMentionViewHelperRender.dart
     asc_page: https://docs.amity.co/social/flutter
     description: Flutter comment user mention rendering with helper
     */
-  void renderTextPostWithUserMention(AmityComment comment) {
+  void renderCommentWithUserMention(AmityComment comment) {
     // parse metadata with the helper
     final mentionedGetter =
         AmityMentionMetadataGetter(metadata: comment.metadata!);
