f186d66a07280f5c259128ab734166bdf046f079
Saurabh kumar <er.sorbh@gmail.com>
2023-03-28 21:14:12 +0530

    fix: [ASC-11752] resolve issue with error handling in private network

--- lib/presentation/screen/user_feed/user_feed_screen.dart
+++ lib/presentation/screen/user_feed/user_feed_screen.dart
@@ -1,12 +1,10 @@
 import 'package:amity_sdk/amity_sdk.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_social_sample_app/core/constant/global_constant.dart';
-import 'package:flutter_social_sample_app/core/widget/dialog/error_dialog.dart';
 import 'package:flutter_social_sample_app/core/widget/feed_widget.dart';
 
 class UserFeedScreen extends StatefulWidget {
-  const UserFeedScreen({Key? key, required this.userId, this.showAppBar = true})
-      : super(key: key);
+  const UserFeedScreen({Key? key, required this.userId, this.showAppBar = true}) : super(key: key);
   final String userId;
   final bool showAppBar;
   @override
@@ -43,8 +41,8 @@ class _UserFeedScreenState extends State<UserFeedScreen> {
           } else {
             //Error on pagination controller
             setState(() {});
-            ErrorDialog.show(context,
-                title: 'Error', message: _controller.error.toString());
+            // ErrorDialog.show(context,
+            //     title: 'Error', message: _controller.error.toString());
           }
         },
       );
@@ -59,9 +57,7 @@ class _UserFeedScreenState extends State<UserFeedScreen> {
   }
 
   void pagination() {
-    if ((scrollcontroller.position.pixels ==
-            scrollcontroller.position.maxScrollExtent) &&
-        _controller.hasMoreItems) {
+    if ((scrollcontroller.position.pixels == scrollcontroller.position.maxScrollExtent) && _controller.hasMoreItems) {
       setState(() {
         _controller.fetchNextPage();
       });
@@ -71,9 +67,7 @@ class _UserFeedScreenState extends State<UserFeedScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: widget.showAppBar
-          ? AppBar(title: Text('User Feed - ${widget.userId}'))
-          : null,
+      appBar: widget.showAppBar ? AppBar(title: Text('User Feed - ${widget.userId}')) : null,
       body: Column(
         children: [
           Container(
@@ -142,13 +136,11 @@ class _UserFeedScreenState extends State<UserFeedScreen> {
                     itemBuilder: (context) {
                       return [
                         PopupMenuItem(
-                          child:
-                              Text(AmityUserFeedSortOption.FIRST_CREATED.name),
+                          child: Text(AmityUserFeedSortOption.FIRST_CREATED.name),
                           value: 2,
                         ),
                         PopupMenuItem(
-                          child:
-                              Text(AmityUserFeedSortOption.LAST_CREATED.name),
+                          child: Text(AmityUserFeedSortOption.LAST_CREATED.name),
                           value: 3,
                         )
                       ];
@@ -180,22 +172,27 @@ class _UserFeedScreenState extends State<UserFeedScreen> {
                       _controller.reset();
                       _controller.fetchNextPage();
                     },
-                    child: ListView.builder(
-                      controller: scrollcontroller,
-                      itemCount: amityPosts.length,
-                      itemBuilder: (context, index) {
-                        final amityPost = amityPosts[index];
-                        return FeedWidget(
-                          amityPost: amityPost,
-                        );
-                      },
-                    ),
+                    child: _controller.error == null
+                        ? ListView.builder(
+                            controller: scrollcontroller,
+                            itemCount: amityPosts.length,
+                            itemBuilder: (context, index) {
+                              final amityPost = amityPosts[index];
+                              return FeedWidget(
+                                amityPost: amityPost,
+                              );
+                            },
+                          )
+                        : Container(
+                            alignment: Alignment.center,
+                            child: Text(_controller.error.toString()),
+                          ),
                   )
                 : Container(
                     alignment: Alignment.center,
                     child: _controller.isFetching
                         ? const CircularProgressIndicator()
-                        : const Text('No Post'),
+                        : Text(_controller.error == null ? 'No Post' : _controller.error.toString()),
                   ),
           ),
           if (_controller.isFetching && amityPosts.isNotEmpty)
