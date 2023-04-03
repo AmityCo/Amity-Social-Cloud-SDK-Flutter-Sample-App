9e66d64573076190eca199ea1669cfaf05adad17
Saurabh kumar <er.sorbh@gmail.com>
2023-04-03 12:48:33 +0530

    fix: [ASC-11752] resolve issue with error handling in private network (#156)

--- lib/presentation/screen/user_post/user_post_screen.dart
+++ lib/presentation/screen/user_post/user_post_screen.dart
@@ -4,8 +4,7 @@ import 'package:flutter_social_sample_app/core/constant/global_constant.dart';
 import 'package:flutter_social_sample_app/core/widget/feed_widget.dart';
 
 class UserPostScreen extends StatefulWidget {
-  const UserPostScreen({Key? key, required this.userId, this.showAppBar = true})
-      : super(key: key);
+  const UserPostScreen({Key? key, required this.userId, this.showAppBar = true}) : super(key: key);
   final bool showAppBar;
   final String userId;
   @override
@@ -45,9 +44,7 @@ class _UserPostScreenState extends State<UserPostScreen> {
   }
 
   void pagination() {
-    if ((scrollcontroller.position.pixels ==
-            scrollcontroller.position.maxScrollExtent) &&
-        _controller.hasMoreItems) {
+    if ((scrollcontroller.position.pixels == scrollcontroller.position.maxScrollExtent) && _controller.hasMoreItems) {
       setState(() {
         _controller.fetchNextPage();
       });
@@ -57,12 +54,10 @@ class _UserPostScreenState extends State<UserPostScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: widget.showAppBar
-          ? AppBar(title: Text('User Post - ${widget.userId}'))
-          : null,
+      appBar: widget.showAppBar ? AppBar(title: Text('User Post - ${widget.userId}')) : null,
       body: amityPosts.isEmpty
-          ? const Center(
-              child: Text('No Post Found'),
+          ? Center(
+              child: Text(_controller.error == null ? 'No Post Found' : _controller.error.toString()),
             )
           : Column(
               children: [
