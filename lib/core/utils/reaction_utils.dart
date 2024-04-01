36b79fde0f0c2411c6dd5afc5a2f2f168f5cd612
Warakorn Sitthirit <warakorn@amity.co>
2024-04-01 18:01:49 +0700

    fix: Fix heart reaction is missing

--- lib/core/utils/reaction_utils.dart
+++ lib/core/utils/reaction_utils.dart
@@ -5,6 +5,8 @@ class ReactionUtils {
         return 'assets/ic_like.png';
       case 'love':
         return 'assets/ic_heart.png';
+      case 'heart':
+        return 'assets/ic_heart.png';
       default:
         return 'assets/ic_like.png';
     }
@@ -15,6 +17,8 @@ class ReactionUtils {
       case 'like':
         return 'assets/ic_liked.png';
       case 'love':
+        return 'assets/ic_heart.png';
+      case 'heart':
         return 'assets/ic_heart.png';
       default:
         return 'assets/ic_liked.png';
